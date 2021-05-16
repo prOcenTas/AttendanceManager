843196583179e47893a2443923d592b1cc9d04dd
Faustas V <55922917+prOcenTas@users.noreply.github.com>
2021-05-16 23:11:32 +0200

    going back to the cleaner code, still mad at that error

--- app/src/main/java/com/example/androidattendance/UserActivitys/TeacherActivitys/Attendance/CheckAttendance.java
+++ app/src/main/java/com/example/androidattendance/UserActivitys/TeacherActivitys/Attendance/CheckAttendance.java
@@ -73,59 +73,57 @@ public class CheckAttendance extends AppCompatActivity {
         studentId=findViewById(R.id.attendanceId);
         attendanceButton=findViewById(R.id.attendanceButton);
 
-
-    }
-
-
-    public void checkIn(View view) {
-        //this studentId.getText().toString() been making me sit and stare at the screen for 2hours because of null
-        //HEY IMMA EXPLAIN WHAT TOOK ME 2HOURS, I HAD A WRONG ID SET ON EDITTEXT STUDENTID.... THATS WHAT TOO MUCH WORK DOES TO YOU
-        //I even rewrote the whole part of check attendance
-
         final DatabaseReference reference= FirebaseDatabase.getInstance().getReference("lecture");
         reference.child(className.getText().toString()).orderByChild("studentId").equalTo(studentId.getText().toString());
 
         final DatabaseReference referenceTo= FirebaseDatabase.getInstance().getReference("attendance")
                 .child(className.getText().toString()).child("Date = " + date.getText().toString()).child(studentId.getText().toString());
-
-
-        reference.child(className.getText().toString()).orderByChild("studentId").equalTo(studentId.getText().toString())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists())
-                        {
-                            ValueEventListener valueEventListener=new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    referenceTo.setValue(snapshot.getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+        //this studentId.getText().toString() been making me sit and stare at the screen for 2hours because of null
+        //HEY IMMA EXPLAIN WHAT TOOK ME 2HOURS, I HAD A WRONG ID SET ON EDITTEXT STUDENTID.... THATS WHAT TOO MUCH WORK DOES TO YOU
+        //I even rewrote the whole part of check attendance
+        attendanceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.child(className.getText().toString()).orderByChild("studentId").equalTo(studentId.getText().toString())
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if(snapshot.exists())
+                                {
+                                    ValueEventListener valueEventListener=new ValueEventListener() {
                                         @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isComplete()) {
-                                                studentId.setText("");
-                                            } else {
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            referenceTo.setValue(snapshot.getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isComplete()) {
+                                                        studentId.setText("");
+                                                    } else {
 
-                                            }
+                                                    }
+                                                }
+                                            });
                                         }
-                                    });
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) { }
+                                    };
+                                    reference.child(className.getText().toString()).child(studentId.getText().toString()).addListenerForSingleValueEvent(valueEventListener);
+                                    counter=counter+1;
+                                    attendanceCounter.setText(String.valueOf(counter));
+                                    Toast.makeText(CheckAttendance.this,"Successful",Toast.LENGTH_SHORT).show();
                                 }
+                                else
+                                {
+                                    Toast.makeText(CheckAttendance.this,"Unsuccessful",Toast.LENGTH_SHORT).show();
+                                }
+                            }
 
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) { }
-                            };
-                            reference.child(className.getText().toString()).child(studentId.getText().toString()).addListenerForSingleValueEvent(valueEventListener);
-                            counter=counter+1;
-                            attendanceCounter.setText(String.valueOf(counter));
-                            Toast.makeText(CheckAttendance.this,"Successful",Toast.LENGTH_SHORT).show();
-                        }
-                        else
-                        {
-                            Toast.makeText(CheckAttendance.this,"Unsuccessful",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {}
-                });
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {}
+                        });
+            }
+        });
     }
+
 }
